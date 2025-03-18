64a5e20a8ea6723d6327c7dd0e258ca0f2529f3c
Kanniyappan062005 <ramyakanniyappan011@gmail.com>
2025-03-18 23:58:50 +0530

    Updated project in menubar

--- collections.js
+++ collections.js
@@ -7,25 +7,6 @@ closeOffermsg.addEventListener("click", function () {
     offerMsg.style.display = "none"
 })
 
-// ***Slide Images Function
-const slideImage = document.querySelector(".slide__image");
-const images = document.querySelectorAll(".slide__image img");
-const prevBtn = document.querySelector(".left-arrow");
-const nextBtn = document.querySelector(".right-arrow");
-let index = 0;
-
-function slide(direction) {
-    if (direction === 'next') {
-        index = (index + 1) % images.length;
-    } else {
-        index = (index - 1 + images.length) % images.length;
-    }
-    slideImage.style.transform = `translateX(-${index * 100}%)`;
-}
-
-prevBtn.addEventListener("click", () => slide('prev'));
-nextBtn.addEventListener("click", () => slide('next'));
-
 // MenuIcon Click Function
 var menuIcon=document.getElementById("menu-icon");
 var menuClose=document.getElementById("menu-close");
